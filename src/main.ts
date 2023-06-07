c32fbde453bd720f4a27bc719cb92109ae654ea0
David Michel <david.michel2809@gmail.com>
2023-06-07 13:36:22 +0200

    feat: sync mouse movements with player position #5

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,8 @@ import { Game } from "./Game"
 
 const game = new Game()
 game.animate()
+
+game.canvas.addEventListener("mousemove", handleMouseMove)
+function handleMouseMove({ offsetX, offsetY }: MouseEvent) {
+  game.player.update({ x: offsetX, y: offsetY })
+}
