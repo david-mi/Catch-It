c64563e44dbc6dcb7876241383d7af8ba7f22385
David Michel <david.michel2809@gmail.com>
2023-06-08 13:09:42 +0200

    refacto: optimization on checkCircleCollision

--- src/helpers/checkCircleCollision.ts
+++ src/helpers/checkCircleCollision.ts
@@ -5,18 +5,9 @@ type Circle = Player | Obstacle
 
 export function checkCircleCollision(circle: Circle, targetCircles: Obstacle[], minGap = 0) {
   for (const targetCircle of targetCircles) {
-    const xMax = Math.max(circle.x, targetCircle.x)
-    const xMin = Math.min(circle.x, targetCircle.x)
-    const xLength = xMax - xMin
-    const xLengthSquared = Math.pow(xLength, 2)
-
-    const yMax = Math.max(circle.y, targetCircle.y)
-    const yMin = Math.min(circle.y, targetCircle.y)
-    const yLength = yMax - yMin
-    const yLengthSquared = Math.pow(yLength, 2)
-
-    const squaredSum = xLengthSquared + yLengthSquared
-    const hypotenuse = Math.sqrt(squaredSum)
+    const xLength = circle.x - targetCircle.x
+    const yLength = circle.y - targetCircle.y
+    const hypotenuse = Math.hypot(xLength, yLength)
 
     const radiusSum = circle.radius + targetCircle.radius + minGap
     if (radiusSum >= hypotenuse) {
